92bd42d6871ca81cb727bd2a4cd344f3edf0cd1f
Larry Yang <2438671+larryang@users.noreply.github.com>
2022-04-29 14:18:18 -0400

    unit test and implement 404 page

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -7,9 +7,10 @@ def test_request_index(client):
     assert b"Hello, World!" in response.data
 
 
-# when base.html is working properly (current_user is undefined error)
-#
-# def test_request_page_not_found(client):
-#     """This tests ./foobar """
-#     response = client.get("/foobar]")
-#     assert response.status_code == 404
+#when base.html is working properly (current_user is undefined error)
+def test_page_not_found(client):
+    """This tests getting a 404 for /foobar """
+    response = client.get("/foobar]")
+    assert response.status_code == 404
+    assert b"<h1>404</h1>" in response.data
+    assert b"Oops! Looks like the page doesn't exist anymore (or never existed)" in response.data
