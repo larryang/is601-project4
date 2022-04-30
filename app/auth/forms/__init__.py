4b02a38fde2bd0896e447faa406efc7d1b955ffe
Larry Yang <2438671+larryang@users.noreply.github.com>
2022-04-30 17:48:47 -0400

    add /register w/ 2 unit test: route and form

--- app/auth/forms/__init__.py
+++ app/auth/forms/__init__.py
@@ -6,12 +6,36 @@ from wtforms.fields import *
 
 class login_form(FlaskForm): # pylint: disable=invalid-name
     """ login form """
-    email = EmailField('Email Address', [
-        validators.DataRequired(),
-    ])
 
-    password = PasswordField('Password', [
-        validators.DataRequired(),
-        validators.length(min=6, max=35)
-    ])
+    email = EmailField('Email Address',
+        [ validators.DataRequired() ]
+        )
+
+    password = PasswordField('Password',
+        [
+            validators.DataRequired(),
+            validators.length(min=6, max=35)
+        ])
+
+    submit = SubmitField()
+
+
+class register_form(FlaskForm): # pylint: disable=invalid-name
+    """ registration form"""
+    email = EmailField('Email Address',
+        [ validators.DataRequired() ],
+        description="Login will be with your email"
+        )
+
+    password = PasswordField('Create Password',
+        [
+            validators.DataRequired(),
+            validators.EqualTo('confirm', message='Passwords must match'),
+        ],
+        description="Create a password "
+        )
+
+    confirm = PasswordField('Repeat Password',
+        description="Please retype your password to confirm it is correct")
+
     submit = SubmitField()
