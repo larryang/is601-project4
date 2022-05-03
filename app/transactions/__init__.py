8570a8536acbff9ba24fc163610c36c1de6baa0b
Larry Yang <2438671+larryang@users.noreply.github.com>
2022-05-02 22:33:32 -0400

    implement and test upload .csv via POST and process into database

--- app/transactions/__init__.py
+++ app/transactions/__init__.py
@@ -6,6 +6,7 @@ from flask import Blueprint, abort, current_app, redirect, render_template, requ
 from flask_login import current_user, login_required
 from flask_wtf import FlaskForm
 from jinja2 import TemplateNotFound
+import werkzeug
 import wtforms
 from app import db
 from app.db.models import Transaction, TransactionTypeEnum
@@ -42,12 +43,34 @@ class CsvUpload(FlaskForm):
 transactions = Blueprint('transactions', __name__,
                         template_folder='templates')
 
+
+@transactions.before_app_first_request
+def setup_upload():
+    """ create upload directory """
+    logdir = current_app.config['UPLOAD_FOLDER']
+    if not os.path.exists(logdir):
+        os.mkdir(logdir)
+
 @transactions.route('/transactions/upload', methods=['POST', 'GET'])
 @login_required
 def transactions_upload():
     """ use form to upload song list CSV """
     log = logging.getLogger("upload_transactions")
     form = CsvUpload()
+
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            filename = werkzeug.utils.secure_filename(form.file.data.filename)
+            filepath = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
+            form.file.data.save(filepath)
+            transactions_list = open_and_parse_csv(filepath)
+
+            current_user.transactions += transactions_list # pylint: disable=assigning-non-slot
+            db.session.commit() # pylint: disable=no-member
+
+            return redirect(url_for('auth.dashboard'))
+        else:
+            log.info("form failed validation")
 
     try:
         return render_template('upload_transactions.j2.html', form=form)
